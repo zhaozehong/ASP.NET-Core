324df976c88c72f1605eb09262a9d31419524ef2
Zehong Zhao <zehong.zhao@HexagonMetrology.com>
2019-06-20 16:07:41 +0800

    be able to DELETE a restaurant

---
+++ DotNetCoreApp/Pages/Restaurants/Delete.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCoreApp.Core;
+using DotNetCoreApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DotNetCoreApp.Pages.Restaurants
+{
+  public class DeleteModel : PageModel
+  {
+    public DeleteModel(IRestaurantData data)
+    {
+      this._data = data;
+    }
+
+    public IActionResult OnGet(int restaurantId)
+    {
+      Restaurant = _data.GetRestaurantById(restaurantId);
+      if (Restaurant == null)
+        return RedirectToPage("./NotFound");
+      return Page();
+    }
+    public IActionResult OnPost(int restaurantId)
+    {
+      var restaurant = _data.Delete(restaurantId);
+      _data.Commit();
+
+      if (restaurant == null)
+        return RedirectToPage("./NotFound");
+
+      TempData["Message"] = $"{restaurant.Name} deleted";
+      return RedirectToPage("./List");
+    }
+
+    public Restaurant Restaurant { get; set; }
+
+    private IRestaurantData _data;
+  }
+}
--- DotNetCoreApp/Pages/Restaurants/List.cshtml.cs
+++ DotNetCoreApp/Pages/Restaurants/List.cshtml.cs
@@ -6,24 +6,29 @@ using DotNetCoreApp.Core;
 using DotNetCoreApp.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace DotNetCoreApp.Pages.Restaurants
 {
   public class ListModel : PageModel
   {
-    public ListModel(IRestaurantData data)
+    public ListModel(IConfiguration config,  IRestaurantData data)
     {
+      _config = config;
       _data = data;
     }
     public void OnGet()
     {
+      Message = _config["Message"];
       Restaurants = _data.GetRestaurantsByName(SearchTerm);
     }
 
     public IEnumerable<Restaurant> Restaurants { get; private set; }
     [BindProperty(SupportsGet = true)]
     public string SearchTerm { get; set; }
+    public string Message { get; set; }
 
+    private readonly IConfiguration _config;
     private readonly IRestaurantData _data;
   }
 }
