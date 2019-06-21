dab9db08012d911695be002b7eed473a157227b8
Zehong Zhao <zehong.zhao@HexagonMetrology.com>
2019-06-21 16:15:23 +0800

    create a CRUD razor page

---
+++ DotNetCoreApp/Pages/R2/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DotNetCoreApp.Core;
+using DotNetCoreApp.Data;
+
+namespace DotNetCoreApp.Pages.R2
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DotNetCoreApp.Data.DotNetCoreAppDbContext _context;
+
+        public CreateModel(DotNetCoreApp.Data.DotNetCoreAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Restaurants.Add(Restaurant);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ DotNetCoreApp/Pages/R2/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DotNetCoreApp.Core;
+using DotNetCoreApp.Data;
+
+namespace DotNetCoreApp.Pages.R2
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DotNetCoreApp.Data.DotNetCoreAppDbContext _context;
+
+        public IndexModel(DotNetCoreApp.Data.DotNetCoreAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Restaurant> Restaurant { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Restaurant = await _context.Restaurants.ToListAsync();
+        }
+    }
+}
